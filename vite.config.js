d08d3909d7b07291f94429953247c73ecf493427
Aaron <piafyoyo06@gmail.com>
2025-10-04 15:48:11 +0800

    feat: 在 vite config 內設定 Unfonts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Unfonts from 'unplugin-fonts/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		Unfonts({
+			google: {
+				display: 'swap',
+				families: [{ name: 'Noto Sans TC', styles: 'wght@400;500;700;900', defer: true }]
+			}
+		})
+	]
 });
